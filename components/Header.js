7badcc40e13bc5acbde05dcc1f79f9e29f7045b5
Luis Jorge <gevolutionxalapa@gmail.com>
2021-11-30 09:50:43 -0600

    Definicion de estilos para la cabecera

--- components/Header.js
+++ components/Header.js
@@ -8,8 +8,14 @@ const Header = () => {
 const styles = StyleSheet.create({
   encabezado: {
     fontFamily: 'Lato-Black',
-    paddingTop: Platform.OS === 'ios' ? 50 : 20,
-    color: '#1c0d02',
+    paddingTop: Platform.OS === 'ios' ? 50 : 25,
+    color: '#FFF',
+    backgroundColor: '#5E49E2',
+    paddingBottom: 25,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    fontSize: 20,
+    marginBottom: 30,
   },
 });
 
