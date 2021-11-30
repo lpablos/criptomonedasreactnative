fca46a4347c98112d7de4f8dbf2595753133c913
Luis Jorge <gevolutionxalapa@gmail.com>
2021-11-30 09:35:54 -0600

    Linkear ka fuente al proyecto

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {StyleSheet, Text, Platform} from 'react-native';
 
 const Header = () => {
-  return <Text style={styles.encabezado}>Criptomonedas</Text>;
+  return (<Text style={styles.encabezado}>Criptomonedas</Text>);
 };
 
 const styles = StyleSheet.create({
   encabezado: {
+    fontFamily: 'Lato-Black',
     paddingTop: Platform.OS === 'ios' ? 50 : 20,
     color: '#1c0d02',
   },
