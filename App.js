aa74af121d073173fe305a9081dc7f4d45507466
Luis Jorge <gevolutionxalapa@gmail.com>
2021-12-01 14:59:42 -0600

    Detalle del fix al cargar los lostados

--- App.js
+++ App.js
@@ -7,27 +7,34 @@
  */
 
 import React from 'react';
+// Librerias
+import {StyleSheet, Image, View} from 'react-native';
 
-import {
-  // SafeAreaView,
-  // ScrollView,
-  // StatusBar,
-  StyleSheet,
-  // Text,
-  // useColorScheme,
-  // View,
-} from 'react-native';
-
+// Components of aplication
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Image style={styles.imagen} source={require('./assets/img/cryptomonedas.png')} />
+      <View style={styles.contenido}>
+        <Formulario />
+      </View>
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imagen: {
+    width: '100%',
+    height: 150,
+    marginHorizontal: '2.5%',
+  },
+  contenido: {
+    marginHorizontal: '3%',
+  },
+});
 
 export default App;
---
+++ components/Formulario.js
@@ -0,0 +1,63 @@
+import React, {useState, useEffect} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import axios from 'axios';
+
+const Formulario = () => {
+  const [moneda, setMoneda] = useState('');
+  //   const [criptomoneda, setCriptomoneda] = useState('');
+  const [criptomonedas, setCriptomonedas] = useState('');
+
+  const obtenerMoneda = moneda => {
+    setMoneda(moneda);
+  };
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url =
+        'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+      const resultado = await axios.get(url);
+      setCriptomonedas(resultado.data.Data);
+    };
+    consultarAPI();
+  }, []);
+
+  return (
+    <View>
+      <Text style={styles.label}>Moneda</Text>
+      <Picker
+        selectedValue={moneda}
+        onValueChange={moneda => obtenerMoneda(moneda)}>
+        <Picker.Item label="Seleccione" value="" />
+        <Picker.Item label="Dolar de estados unidos" value="USD" />
+        <Picker.Item label="Peso Méxicano" value="MXN" />
+        <Picker.Item label="Euro" value="EUR" />
+        <Picker.Item label="Libra Esterlina" value="GBP" />
+      </Picker>
+      <Text style={styles.label}>Criptomoneda</Text>
+      <Picker
+        selectedValue={moneda}
+        onValueChange={moneda => obtenerMoneda(moneda)}>
+        <Picker.Item label="Seleccione" value="" />
+        {
+          criptomonedas.map(cripto=>(
+            <Picker.Item key={cripto.CoinInfo.Id} label={cripto.CoinInfo.FullName} value={cripto.CoinInfo.Name} />
+            
+          ))
+        }
+      </Picker>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  label: {
+    color: '#454545',
+    fontFamily: 'Lato-Black',
+    textTransform: 'uppercase',
+    fontSize: 22,
+    marginVertical: 20,
+  },
+});
+
+export default Formulario;
